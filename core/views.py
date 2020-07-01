79e2a6d31b1da2d7c31d19d630c5f52f63f771c2
FlyvioJR <flaviotaylorr@gmail.com>
2020-07-01 11:48:12 -0300

    tela de criacao de eventos adicionada

--- core/views.py
+++ core/views.py
@@ -25,7 +25,7 @@ def submit_login(request):
             login(request, usuario)
             return redirect('/')
         else:
-             messages.error(request, "Usuario ou senha invalido")
+            messages.error(request, "Usuario ou senha invalido")
     return redirect('/')
 
 
@@ -37,5 +37,17 @@ def lista_eventos(request):
     return render(request, 'agenda.html', dados)
 
 
-# def index(request):
-#    return redirect('/agenda/')
+@login_required(login_url='/login/')
+def evento_tela(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+    return redirect('/')
